0efd2cbfdff1e0eae8681da33969996b54c5ad76
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-05-18 22:53:54 +0100

    test commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,7 +17,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+
         <Search />
+        
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
