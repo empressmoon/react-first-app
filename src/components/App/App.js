4221888755e962d8c89ceb070edcff761cb25ce9
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-05-20 18:59:28 +0100

    Use webpack server in taskrunner and heroku

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,6 +21,7 @@ const App = () => (
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
         <Route exact path="/list/:id" component={List} />
+
       </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
